0c08d75627636644027481f47b98c631aef8c0d6
Paul Grandperrin <paul.grandperrin@gmail.com>
2019-11-27 12:17:47 +0000

    add build_linux

---
+++ build_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -xe
+
+if [ -d linux-hardened ]; then
+  cd linux-hardened
+  git fetch
+else
+  git clone https:////github.com/anthraxx/linux-hardened.git
+  cd linux-hardened
+fi
+
+git checkout 5.4
+
+./scripts/kconfig/merge_config.sh -m -r -O . ../config-firecracker-4.14.72 ../config-linux-hardened
+make olddefconfig
+make vmlinux -j8
