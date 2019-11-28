5eba09fbd0ccf4f3d57470fc44ce944969ad79b2
Paul Grandperrin <paul.grandperrin@gmail.com>
2019-11-28 00:32:58 +0000

    add build_firecracker

--- build_firecracker.sh
+++ build_firecracker.sh
@@ -1,77 +1,50 @@
 #!/bin/sh
 set -xe
 
+#ip tuntap add tap0 mode tap
+#ip addr add 172.16.0.1/24 dev tap0
+#ip link set tap0 up
+#sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -i tap0 -o eth0 -j ACCEPT
+#
+#
+#echo "in guest:"
+#echo "ip addr add 172.16.0.2/24 dev eth0"
+#echo "ip link set eth0 up"
+#echo "ip route add default via 172.16.0.1 dev eth0"
+#echo "echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
+#
 
+cp microvm/90-microvm.network /etc/systemd/network/
+systemctl enable systemd-networkd
+systemctl restart systemd-networkd
 
+umount -q mdir || true
+rm -f rootfs.ext4
+dd if=/dev/zero of=rootfs.ext4 bs=1M count=1000
+mkfs.ext4 rootfs.ext4
+mkdir -p mdir
+mount rootfs.ext4 mdir
+container=mdir
 
-arch=`uname -m`
-kernel_path="hello-vmlinux.bin"
+debootstrap --include=ca-certificates buster $container
+echo 'deb http://security.debian.org/ buster/updates main contrib non-free' >> $container/etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian buster-updates main contrib non-free' >> $container/etc/apt/sources.list
+#echo 'deb [trusted=yes] file:///repo ./' >> $container/etc/apt/sources.list
+#cp -rv repo $container/
+systemd-nspawn -D $container -E DEBIAN_FRONTEND=noninteractive apt update -y
+systemd-nspawn -D $container -E DEBIAN_FRONTEND=noninteractive apt upgrade -y
+systemd-nspawn -D $container -E DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y
+systemd-nspawn -D $container systemctl enable systemd-networkd.service
+systemd-nspawn -D $container systemctl enable systemd-resolved.service
+cp microvm/eth0.network $container/etc/systemd/network/ # override default conf to remove LinkLocalAddressing which conflicts with GCP internal DNS address #sad
+cp microvm/serial-getty@ttyS0.service $container/etc/systemd/system/
+ln -sf /run/systemd/resolve/stub-resolv.conf $container/etc/resolv.conf
 
-if [ ${arch} = "x86_64" ]; then
-  curl --unix-socket /tmp/firecracker.socket -i \
-      -X PUT 'http://localhost/boot-source'   \
-      -H 'Accept: application/json'           \
-      -H 'Content-Type: application/json'     \
-      -d "{
-            \"kernel_image_path\": \"${kernel_path}\",
-            \"boot_args\": \"console=ttyS0 reboot=k panic=1 pci=off\"
-       }"
-elif [ ${arch} = "aarch64" ]; then
-    curl --unix-socket /tmp/firecracker.socket -i \
-      -X PUT 'http://localhost/boot-source'   \
-      -H 'Accept: application/json'           \
-      -H 'Content-Type: application/json'     \
-      -d "{
-            \"kernel_image_path\": \"${kernel_path}\",
-            \"boot_args\": \"keep_bootcon console=ttyS0 reboot=k panic=1 pci=off\"
-       }"
-else
-    echo "Cannot run firecracker on $arch architecture!"
-    exit 1
-fi
+umount mdir
 
-rootfs_path="hello-rootfs.ext4"
-curl --unix-socket /tmp/firecracker.socket -i \
-  -X PUT 'http://localhost/drives/rootfs' \
-  -H 'Accept: application/json'           \
-  -H 'Content-Type: application/json'     \
-  -d "{
-        \"drive_id\": \"rootfs\",
-        \"path_on_host\": \"${rootfs_path}\",
-        \"is_root_device\": true,
-        \"is_read_only\": false
-   }"
-
-ip tuntap add tap0 mode tap
-ip addr add 172.16.0.1/24 dev tap0
-ip link set tap0 up
-sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i tap0 -o eth0 -j ACCEPT
-
-curl -X PUT \
-  --unix-socket /tmp/firecracker.socket \
-  http://localhost/network-interfaces/eth0 \
-  -H accept:application/json \
-  -H content-type:application/json \
-  -d '{
-      "iface_id": "eth0",
-      "guest_mac": "AA:FC:00:00:00:01",
-      "host_dev_name": "tap0"
-    }'
-
-echo "in guest:"
-echo "ip addr add 172.16.0.2/24 dev eth0"
-echo "ip link set eth0 up"
-echo "ip route add default via 172.16.0.1 dev eth0"
-echo "echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
-
-curl --unix-socket /tmp/firecracker.socket -i \
-    -X PUT 'http://localhost/actions'       \
-    -H  'Accept: application/json'          \
-    -H  'Content-Type: application/json'    \
-    -d '{
-        "action_type": "InstanceStart"
-     }'
-
+wget https://github.com/firecracker-microvm/firecracker/releases/download/v0.19.0/firecracker-v0.19.0 -O firecracker
+chmod +x firecracker
+./firecracker --no-api --config-file microvm/config.json
