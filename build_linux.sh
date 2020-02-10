7b362770550df49617ebc2be39469645087ec96f
Paul (PaulG) Grandperrin <paul.grandperrin@gmail.com>
2020-02-10 01:08:10 +0100

    Rebase hardened patches against latest stable tree

--- build_linux.sh
+++ build_linux.sh
@@ -5,14 +5,17 @@ apt install -y build-essential flex bison bc libssl-dev libelf-dev
 
 if [ -d linux-hardened ]; then
   cd linux-hardened
-  git fetch
 else
   git clone https://github.com/anthraxx/linux-hardened.git
   cd linux-hardened
+  git remote add stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+  git checkout -b custom
 fi
 
-git checkout 5.5
+git fetch --all
+git checkout custom
 git reset --hard origin/5.5
+git rebase stable/linux-5.5.y
 
 ./scripts/kconfig/merge_config.sh -m -r -O . ../config-firecracker-4.14.72 ../config-linux-hardened
 make olddefconfig
